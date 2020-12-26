649879f44249779b0bb8208ed5ed432d0ea42cca
Lina Zughaiyer <linazughaiyer@hotmail.com>
2020-12-26 15:09:33 -0500

    add employee/engineer code

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,8 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
