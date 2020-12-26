b9bf6a1c1bd5e9461a08092d14b8ae550a65d6ed
Lina Zughaiyer <linazughaiyer@hotmail.com>
2020-12-26 15:11:01 -0500

    add code to intern js

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,8 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
