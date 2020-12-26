d207f7a1c7d192a891fbbdfdb98d14018cb65c77
Lina Zughaiyer <linazughaiyer@hotmail.com>
2020-12-26 13:54:57 -0500

    added new code to engineer

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,8 @@
 // TODO: Write code to define and export the Employee class
+class Engineer {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
