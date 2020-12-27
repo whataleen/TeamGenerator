868aa0508a1d98480c2fb33a4de97d21663d9030
Lina Zughaiyer <linazughaiyer@hotmail.com>
2020-12-26 22:11:12 -0500

    added school constructor

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,8 +1,8 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 class Engineer extends Employee {
-    constructor(name, id, email, githubProfile) {
+    constructor(name, id, email, github) {
         super(name, id, email);
-        this.githubProfile = githubProfile;
+        this.github = github;
     }
 }
 
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,7 +1,8 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 class Intern extends Employee {
-    constructor(name, id, email) {
+    constructor(name, id, email, school) {
         super(name, id, email);
+        this.school = school;
     }
 }
 
